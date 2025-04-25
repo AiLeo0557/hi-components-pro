d28b68c5b2a40c8f1388bf9717fe3f8cc2de567b
AiLeo0557 <leo1949@dingtalk.com>
2025-04-25 10:43:07 +0800

    feat: 添加 HiInput 组件及其输入功能

---
+++ src/components/HiInput/HiInput.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ElInput } from "element-plus";
+// import { buttonProps } from "element-plus/es/components/button/src/button";
+
+// 继承 Element-Button 的所有 Props
+// const props = defineProps({
+//   ...buttonProps,
+//   // 扩展自定义 Props
+//   rounded: {
+//     type: Boolean,
+//     default: false,
+//   },
+// });
+
+// const props = defineProps({
+//   ...inputProps,
+// });
+
+// 透传所有 Element-Button 的功能
+defineOptions({
+  name: "HiInput",
+  // inheritAttrs: false,
+});
+</script>
+
+<template>
+  <ElInput v-bind="$attrs" />
+</template>
+
+<style scoped>
+</style>

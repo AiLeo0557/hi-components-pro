acb9bec33463052f97e5a1ea194d0d4c63e0bd20
AiLeo0557 <leo1949@dingtalk.com>
2025-04-27 16:55:52 +0800

    feat: 重构组件导入方式以增强可读性和维护性

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
-export { default as HButton } from './components/HButton/HButton.vue'
-export { default as HiInput } from './components/HiInput/HiInput.vue'
-export { default as HiSelect } from './components/HiSelect/HiSelect.vue'
-export { default as HiDate } from './components/HiDate/HiDate.vue'
-export { default as HiFormItem } from './components/HiFormItem/HiFormItem.vue'
-export { default as HiForm } from './components/HiForm/HiForm.vue'
-export { default as HiTable } from './components/HiTable/HiTable.vue'
-export { default as HiTableItem } from './components/HiTable/HiTableItem'
-export { default as HiTableGroup } from './components/HiTable/HiTableGroup'
+import { default as HiInput } from './components/HiInput/HiInput.vue'
+import { default as HiSelect } from './components/HiSelect/HiSelect.vue'
+import { default as HiDate } from './components/HiDate/HiDate.vue'
+import { default as HiFormItem } from './components/HiFormItem/HiFormItem.vue'
+import { default as HiForm } from './components/HiForm/HiForm.vue'
+import { default as HiTable } from './components/HiTable/HiTable.vue'
+import { default as HiTableItem } from './components/HiTable/HiTableItem'
+import { default as HiTableGroup } from './components/HiTable/HiTableGroup'
+export default {
+  install(app: any) {
+    app.component(HiInput.name, HiInput)
+    app.component(HiSelect.name, HiSelect)
+    app.component(HiDate.name, HiDate)
+    app.component(HiFormItem.name, HiFormItem)
+    app.component(HiForm.name, HiForm)
+    app.component(HiTable.name, HiTable)
+    app.component(HiTableItem.name, HiTableItem)
+    app.component(HiTableGroup.name, HiTableGroup)
+  }
+}

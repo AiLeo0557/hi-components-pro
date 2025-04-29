9e3604ffc9d026d59ae47334d34d91c7f55f11fb
AiLeo0557 <leo1949@dingtalk.com>
2025-04-29 16:55:59 +0800

    feat: 新增 IconPointer 组件，提供数据图表图例指示器样式

---
+++ src/components/HiIcon/IconPointer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div :style="`background: ${$attrs.color}`" class="data-chart-legend-pointer"></div>
+</template>
+<style lang="scss" scoped>
+.data-chart-legend-pointer {
+  height: 10px;
+  width: 10px;
+  border-radius: 50%;
+  display: inline-block;
+  margin-right: 6px;
+}
+</style>

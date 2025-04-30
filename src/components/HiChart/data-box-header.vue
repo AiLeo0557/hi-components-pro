7c53b29fec434bbc7e6946128a56a5e155432be5
AiLeo0557 <leo1949@dingtalk.com>
2025-04-30 15:23:21 +0800

    feat: 添加 data-box-header 组件，优化数据展示结构

---
+++ src/components/HiChart/data-box-header.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup></script>
+<template>
+  <header class="data-box-header">
+    <h1 class="_title">
+      <el-icon size="16">
+        <icon-header-bd />
+      </el-icon>
+      <span class="_text">{{ $attrs.title }}</span>
+    </h1>
+    <slot></slot>
+    <span v-if="$attrs.unit" class="_unit"> 单位：{{ $attrs.unit }} </span>
+  </header>
+</template>
+<style lang="scss" scoped>
+.data-box-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  ._title {
+    display: flex;
+    align-items: center;
+    margin-left: -5px;
+    ._text {
+      font-size: 16px;
+      font-weight: 500;
+    }
+  }
+  ._unit {
+    color: #fffffff2;
+    font-size: 14px;
+  }
+}
+</style>

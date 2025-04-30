185fc2a7d20e54cd39d0769376837b55162adb08
AiLeo0557 <leo1949@dingtalk.com>
2025-04-30 15:23:37 +0800

    feat: 添加 IconHeaderBd 组件，提供图标展示功能

---
+++ src/components/HiIcon/IconHeaderBd.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 4 18" class="design-iconfont">
+    <path d="M0 0.5L4 2.5V15.5L0 17.5V0.5Z" fill="#00F0FF" />
+  </svg>
+</template>
